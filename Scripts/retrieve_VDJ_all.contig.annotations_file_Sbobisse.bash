d76a493c80016b7c0f257d24d12bd6b8eaa14c51
Yuqia <37029009+Yuqia@users.noreply.github.com>
2024-09-06 13:37:57 +0200

    Delete Scripts directory

--- Scripts/retrieve_VDJ_all.contig.annotations_file_Sbobisse.bash
+++
@@ -1,9 +0,0 @@
-# copy all_contig_annotations files of cell ranger
-
-here=/home/localadmin/Desktop/RESEARCH/SBobisse/cellranger/VDJ
-
-for i in `ls $here`
-do
-	cp $here/$i/outs/all_contig_annotations.csv $here/${i}_all_contig_annotation.csv
-done
-	
